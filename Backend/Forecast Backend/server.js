067ef1e47d22544fa3b9d91a0742d6b403852b44
NISHITSUMAN <nishitkumar105@outlook.com>
2025-10-07 19:11:36 +0530

    render

--- Backend/Forecast Backend/server.js
+++ Backend/Forecast Backend/server.js
@@ -8,7 +8,7 @@ const { generate5DayForecast } = require('./services/forecastService');
 const { aqiCategory } = require('./services/aqiService');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
